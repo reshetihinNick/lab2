9a4ce47682f75c674d03cf82488b86268261cefd
reshe <reshetihink@mail.ru>
2023-03-24 16:29:25 +0300

    Исправил тесты

--- src/test/java/FindTest.java
+++ src/test/java/FindTest.java
@@ -5,6 +5,7 @@ import org.Find.Find;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 
 public class FindTest {
     Find find = new Find();
@@ -13,47 +14,46 @@ public class FindTest {
     boolean subDirSearch = false;
 
     private String outputString(File searchDir, String searchFileName, boolean subDirSearch) {
-        String output;
+        String[] output;
+        StringJoiner result = new StringJoiner("\r\n");
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
         find.findFile(searchDir, searchFileName, subDirSearch);
-        output = outputStream.toString();
+        output = outputStream.toString().split("\r\n");
+        for (String lines: output) {
+            result.add(lines.substring(lines.indexOf(".")));
+        }
         System.setOut(null);
-        return output;
+        return result.toString();
     }
 
     @Test
     public void find() {
         assertEquals(outputString(searchDir, searchFileName, subDirSearch),
                 """
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\file1.docx\r
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\file1.txt\r
-                        """);
+                        .\\files\\file1.docx\r
+                        .\\files\\file1.txt""");
         subDirSearch = true;
         assertEquals(outputString(searchDir, searchFileName, subDirSearch),
                 """
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\dir1\\file1.txt\r
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\file1.docx\r
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\file1.txt\r
-                        """);
+                        .\\files\\dir1\\file1.txt\r
+                        .\\files\\file1.docx\r
+                        .\\files\\file1.txt""");
         searchFileName = "*.docx";
         assertEquals(outputString(searchDir, searchFileName, subDirSearch),
                 """
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\file1.docx\r
-                        """);
+                        .\\files\\file1.docx""");
         searchFileName = "*";
         assertEquals(outputString(searchDir, searchFileName, subDirSearch),
                 """
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\dir1\\file1.txt\r
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\file1.docx\r
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\file1.txt\r
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\file2.txt\r
-                        """);
+                        .\\files\\dir1\\file1.txt\r
+                        .\\files\\file1.docx\r
+                        .\\files\\file1.txt\r
+                        .\\files\\file2.txt""");
         searchFileName = "file1.txt";
         assertEquals(outputString(searchDir, searchFileName, subDirSearch),
                 """
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\dir1\\file1.txt\r
-                        C:\\Users\\reshe\\IdeaProjects\\lab2\\.\\files\\file1.txt\r
-                        """);
+                        .\\files\\dir1\\file1.txt\r
+                        .\\files\\file1.txt""");
     }
 }
